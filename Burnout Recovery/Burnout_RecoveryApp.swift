f069dc159b1f8d8c289486a0fec49c3c844d3de2
rolandbiro <biro.roland@gmail.com>
2025-12-12 12:44:40 +0100

    Initial Commit

---
+++ Burnout Recovery/Burnout_RecoveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Burnout_RecoveryApp.swift
+//  Burnout Recovery
+//
+//  Created by Biro Roland on 2025. 12. 12..
+//
+
+import SwiftUI
+
+@main
+struct Burnout_RecoveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Burnout Recovery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Burnout Recovery
+//
+//  Created by Biro Roland on 2025. 12. 12..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
